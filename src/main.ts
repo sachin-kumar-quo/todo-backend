3ad7568c81df653e34eaace11d67c88f94a25cd4
sachin <sachin.kumar@quovantis.com>
2022-02-08 16:47:50 +0530

    port added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('PORT');
   app.enableCors({
     origin: '*',
     credentials: true,
   });
-  await app.listen(8000);
+  await app.listen(port | 8000);
 }
 bootstrap();
